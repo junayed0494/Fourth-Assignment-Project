d983c9f704825e0bcca5edf61dc3a598c1850e8d
junayed0494 <junayed0494@gmail.com>
2026-02-26 19:04:45 +0500

    here added some functionality

---
+++ script.js
@@ -0,0 +1,72 @@
+let currentTab = 'all';
+let tabActive = ["bg-blue-600", "text-white"];
+let tabInActive = ["bg-transparent", "text-slate-700", "border-slate-200"];
+
+const cardContainer = document.getElementById('card-container');
+const interviewContainer = document.getElementById('interview-container');
+const rejectContainer = document.getElementById('reject-container');
+const emptyState = document.getElementById('empty-state');
+
+function switchTab(tab){
+    const tabs = ['all', 'interview', 'reject'];
+    currentTab = tab;
+
+    for(const t of tabs){
+        const tabName = document.getElementById('btn-' + t);
+        console.log(tabName);
+
+         if(t === tab){
+            tabName.classList.remove(...tabInActive);
+            tabName.classList.add(...tabActive);
+        }
+        else{
+            tabName.classList.add(...tabInActive);
+            tabName.classList.remove(...tabActive);
+        }
+        //-------------------------------------------
+                    
+    }
+
+    const allSection = [cardContainer, interviewContainer, rejectContainer];
+
+    for(const section of allSection){
+        section.classList.add('hidden');
+    }
+
+    emptyState.classList.add("hidden");
+
+    if(tab === 'all'){
+           cardContainer.classList.remove("hidden"); 
+           if(cardContainer.children.length < 1){
+            emptyState.classList.remove("hidden");
+           }
+        }
+        else if(tab === 'interview'){
+            interviewContainer.classList.remove("hidden");
+            if(interviewContainer.children.length < 1){
+                emptyState.classList.remove("hidden");
+            }
+        }
+        else{
+            rejectContainer.classList.remove("hidden");
+            if(rejectContainer.children.length < 1){
+                emptyState.classList.remove("hidden");
+            }
+        }
+    updateStat();
+}
+
+
+
+const countTotal = document.getElementById('count-total');
+const countInterview = document.getElementById('count-interview');
+const countReject = document.getElementById('count-reject');
+const countAvailable = document.getElementById('count-available');
+
+countTotal.innerText = cardContainer.children.length;
+
+switchTab(currentTab);
+
+
+
+
